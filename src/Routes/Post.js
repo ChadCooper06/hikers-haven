df53752213e62067b28479d95c6ba821f56aad56
ChadCooper06 <chad.cooper@student.awesomeinc.org>
2022-04-29 14:26:03 +0000

    Added comments to CSS and React Portal

--- src/Routes/Post.js
+++ src/Routes/Post.js
@@ -2,18 +2,18 @@ import React from "react";
 import Comment from './Comment';
 
 const Post = ({ comments, setComments }) => {
-  return (
-      <div className="post-container">
-          <div className="post-list">
-             {comments.map((comment) => (
-                 <Comment key={comment.id} 
-                 comment={comment} 
-                 comments={comments} 
-                 setCommentss={setComments} />
-             ))}
-          </div>
-      </div>
-  ) 
+    return (
+        <div className="post-container">
+          
+            {comments.map((comment) => (
+                <Comment key={comment.id} 
+                comment={comment} 
+                comments={comments} 
+                setCommentss={setComments} />
+            ))}
+          
+        </div>
+    ) 
 }
 
 export default Post;
